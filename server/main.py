fdd3103717de03fd589e58668e547a2f05829866
Step2Victory <stepanmalov555@gmail.com>
2023-11-25 09:54:29 +0000

    add

--- server/main.py
+++ server/main.py
@@ -21,127 +21,129 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/buildings")
-def main():
-    global model
-
-    return [{
-        "latitude": 55.733364,
-        "longitude": 37.640751,
-        "geocoderAddress": "Addr",
-        "buildingAge": 29,
-        "buildingSquare": 230,
-        "technicalConditions": "Хорошее",
-        "krValue": 21212124,
-        "ksValue": 2112124,
-        "trValue": 123123,
-        "residualValue": 2011000,
-        "balanceValue": 2012120,
-    }, {
-        "latitude": 55.741557,
-        "longitude": 37.620027,
-        "geocoderAddress": "Some addr",
-        "buildingAge": 29,
-        "buildingSquare": 230,
-        "technicalConditions": "Хорошее",
-        "krValue": 211212124, # кап ремонт
-        "ksValue": 21122124, # еще какие-то
-        "trValue": 1232123, # устройство территории
-        "residualValue": 20110010,
-        "balanceValue": 20122120,
-    }]
-
-
-@app.post("/predict")
-def get_info(row: dict):
-
-    return [{
-        "date": "2022-10-01T00:00:00",
-        "prediction": 100000,
-        "weatherMin": 20,
-        "weatherMax": 30,
-        "weatherAvg": 30,    
-        "precipitation": 25,
-    },
-    {
-        "date": "2022-11-01T00:00:00",
-        "prediction": 10000,
-        "weatherMin": 19,
-        "weatherMax": 32,
-        "weatherAvg": 25,    
-        "precipitation": 25,
-    },
-    {
-        "date": "2022-12-01T00:00:00",
-        "prediction": 10000,
-        "weatherMin": 19,
-        "weatherMax": 32,
-        "weatherAvg": 25,    
-        "precipitation": 25,
-    }]
-
-
-@app.get("/buildings/{id}")
-def buildings(id):
-    id = 'Россия, Еврейская автономная область, Биробиджан, проспект 60-летия СССР, 5'
-    return [{
-        "date": "2022-10-01T00:00:00",
-        "prediction": 100000,
-        "weatherMin": 20,
-        "weatherMax": 30,
-        "weatherAvg": 30,    
-        "precipitation": 25,
-    },
-    {
-        "date": "2022-11-01T00:00:00",
-        "prediction": 10000,
-        "weatherMin": 19,
-        "weatherMax": 32,
-        "weatherAvg": 25,    
-        "precipitation": 25,
-    },
-    {
-        "date": "2022-12-01T00:00:00",
-        "prediction": 10000,
-        "weatherMin": 19,
-        "weatherMax": 32,
-        "weatherAvg": 25,    
-        "precipitation": 25,
-    }]
-
-
 # @app.get("/buildings")
 # def main():
 #     global model
-#     return model.get_buildings()
 
-
-
+#     return [{
+#         "latitude": 55.733364,
+#         "longitude": 37.640751,
+#         "geocoderAddress": "Addr",
+#         "buildingAge": 29,
+#         "buildingSquare": 230,
+#         "technicalConditions": "Хорошее",
+#         "krValue": 21212124,
+#         "ksValue": 2112124,
+#         "trValue": 123123,
+#         "residualValue": 2011000,
+#         "balanceValue": 2012120,
+#     }, {
+#         "latitude": 55.741557,
+#         "longitude": 37.620027,
+#         "geocoderAddress": "Some addr",
+#         "buildingAge": 29,
+#         "buildingSquare": 230,
+#         "technicalConditions": "Хорошее",
+#         "krValue": 211212124, # кап ремонт
+#         "ksValue": 21122124, # еще какие-то
+#         "trValue": 1232123, # устройство территории
+#         "residualValue": 20110010,
+#         "balanceValue": 20122120,
+#     }]
 
 
 # @app.post("/predict")
 # def get_info(row: dict):
-#     # row = {
-#     #     "latitude": 55.733364,
-#     #     "longitude": 37.640751,
-#     #     "geocoderAddress": "Addr",
-#     #     "buildingAge": 29,
-#     #     "buildingSquare": 230,
-#     #     "technicalConditions": "Хорошее",
-#     #     "krValue": 21212124,
-#     #     "ksValue": 2112124,
-#     #     "trValue": 123123,
-#     #     "residualValue": 2011000,
-#     #     "balanceValue": 2012120,
-#     # }
-#     global model
-#     print(row)
-#     pred = model.predict(row)
-#     print(pred)
-#     return pred
+
+#     return [{
+#         "date": "2022-10-01T00:00:00",
+#         "prediction": 100000,
+#         "weatherMin": 20,
+#         "weatherMax": 30,
+#         "weatherAvg": 30,    
+#         "precipitation": 25,
+#     },
+#     {
+#         "date": "2022-11-01T00:00:00",
+#         "prediction": 10000,
+#         "weatherMin": 19,
+#         "weatherMax": 32,
+#         "weatherAvg": 25,    
+#         "precipitation": 25,
+#     },
+#     {
+#         "date": "2022-12-01T00:00:00",
+#         "prediction": 10000,
+#         "weatherMin": 19,
+#         "weatherMax": 32,
+#         "weatherAvg": 25,    
+#         "precipitation": 25,
+#     }]
+
 
 # @app.get("/buildings/{id}")
 # def buildings(id):
 #     id = 'Россия, Еврейская автономная область, Биробиджан, проспект 60-летия СССР, 5'
-#     global model
-#     return model.predict_by_id(id)
+#     return [{
+#         "date": "2022-10-01T00:00:00",
+#         "prediction": 100000,
+#         "weatherMin": 20,
+#         "weatherMax": 30,
+#         "weatherAvg": 30,    
+#         "precipitation": 25,
+#     },
+#     {
+#         "date": "2022-11-01T00:00:00",
+#         "prediction": 10000,
+#         "weatherMin": 19,
+#         "weatherMax": 32,
+#         "weatherAvg": 25,    
+#         "precipitation": 25,
+#     },
+#     {
+#         "date": "2022-12-01T00:00:00",
+#         "prediction": 10000,
+#         "weatherMin": 19,
+#         "weatherMax": 32,
+#         "weatherAvg": 25,    
+#         "precipitation": 25,
+#     }]
+
+
+@app.get("/buildings")
+def main():
+    global model
+    return model.get_buildings()
+
+
+
+
+@app.post("/predict")
+def get_info(row: dict):
+    # row = {
+    #     "latitude": 55.733364,
+    #     "longitude": 37.640751,
+    #     "geocoderAddress": "Addr",
+    #     "buildingAge": 29,
+    #     "buildingSquare": 230,
+    #     "technicalConditions": "Хорошее",
+    #     "krValue": 21212124,
+    #     "ksValue": 2112124,
+    #     "trValue": 123123,
+    #     "residualValue": 2011000,
+    #     "balanceValue": 2012120,
+    # }
+    global model
+    print(row)
+    pred = model.predict(row)
+    print(pred)
+    return pred
+
+@app.get("/buildings/{id}")
+def buildings(id):
+    print(id)
+    # id = 'Россия, Еврейская автономная область, Биробиджан, проспект 60-летия СССР, 5'
+    global model
+
+    return model.predict_by_id(id)
+    
--- server/model.py
+++ server/model.py
@@ -1,37 +1,57 @@
 import numpy as np
 import pandas as pd
+import math
+import json
+import requests
+import datetime
+from meteostat import Point, Monthly
 from catboost import CatBoostRegressor, Pool
+from dateutil.relativedelta import relativedelta
 
-from  timeseries_train import train_models, predict 
 
 
-def _compute_buildings(data):
-    buildings = dict()
-    processed = set()
-    for _, row in data.iterrows():
-        geocoder_address = row["geocoder_address"]
-        if geocoder_address in processed:
-            continue
-        row_to_insert = dict(
-            latitude=row["geocoder_lat"],
-            longitude=row["geocoder_lon"],
-            buildingAge=row["ВОЗРАСТ ЗДАНИЯ"],
-            buildingSquare=row["Площадь объекта недвижимости \nкв. м"],
-            technicalConditions=row["Техническое состояние"],
-            krValue=row["amount_money_kr"],
-            trValue=row["amount_money_ks"],
-            ksValue=row["amount_money_ks"],
-        )
-        buildings[geocoder_address] = row_to_insert
-    return buildings
+def _get_datetime_range(start, end):
+    result = []
+    while start < end:
+        result.append(start)
+        start += relativedelta(months=1)
+    return result
+
+def _replace_nan(num):
+    if pd.isna(num):
+        return None
+    return num
 
 class Model:
 
     def __init__(self,
                  path_to_train,
         ):
-        features = ["month", # месяц
-            "TARGET", # prediction
+        self.model_to_front = {
+            "month": "month",
+            "geocoder_lat": "latitude",
+            "geocoder_lon": "longitude",
+            "geocoder_address": "geocoderAddress",
+            "amount_money_kr": "krValue", # kr
+            "amount_money_ks": "ksValue", # ks
+            "amount_money_tr": "trValue", # tr
+            "tavg": "weatherAvg", 
+            "tmin": "weatherMin",
+            "tmax": "weatherMax",
+            "prcp": "precipitation", # осадки
+            "Техническое состояние": "technicalConditions",
+            "ВОЗРАСТ ЗДАНИЯ": "buildingAge",
+            "Площадь объекта недвижимости \nкв. м": "buildingSquare",
+            "Остаточная стоимость, руб.": "residualValue",
+            "Балансовая стоимость, руб.": "balanceValue",
+            "TARGET": "prediction",
+        }
+        self.front_to_model = {
+            value: key for key, value in self.model_to_front.items()
+        }
+        print(self.front_to_model)
+        self.features = [
+            "month", # месяц
             "geocoder_lat", # lat
             "geocoder_lon", # lon
             "amount_money_kr", # kr
@@ -53,34 +73,130 @@ class Model:
         target = "TARGET"
         data = pd.read_feather(path_to_train)
         self.target = data[target]
-        self.train = data[features]
+        self.train = data[self.features]
         for col in cat_features:
             self.train[col] = self.train[col].fillna("")
         self.model = CatBoostRegressor().fit(
-                X=self.train,
-                y=self.target,
-                cat_features=cat_features,
+            X=self.train,
+            y=self.target,
+            cat_features=cat_features,
+
         )
-        self.buildings = _compute_buildings(data)
+
+        self.buildings = self._compute_buildings(data)
         self.train_predictions = self.model.predict(self.train)
+        pd.DataFrame(self.train_predictions, columns=["TARGET"]).to_feather("predictions.frt")
+    
+
+    def _compute_buildings(self, data):
+        buildings = dict()
+        processed = set()
+        columns = [
+            "latitude",
+            "longitude",
+            "buildingAge",
+            "buildingSquare",
+            "technicalConditions",
+            "krValue",
+            "trValue",
+            "ksValue",
+            "residualValue",
+            "balanceValue",
+        ]
+        for _, row in data.iterrows():
+            geocoder_address = row["geocoder_address"]
+            if geocoder_address in processed:
+                continue
+            row_to_insert = {
+                column: row[self.front_to_model[column]]
+                for column in columns
+            }
+            buildings[geocoder_address] = row_to_insert
+        return buildings
 
 
     def get_buildings(self):
         res = []
+        
         for geocoder_address, building in self.buildings.items():
             out = building.copy()
             out["geocoderAddress"] = geocoder_address
+            out = {
+                key: _replace_nan(value) for key, value in out.items()
+            }
             res.append(out)
-        return out
 
-    def get_features(self, geocoder_address):
-        return self.train.loc[geocoder_address]
+        return res
 
+    def _request_to_weather_api(self, start, end, lat, lon):
+        point = Point(lat=lat, lon=lon)
+        data = Monthly(point, start, end)
+        data = data.fetch()
+        if not len(data):
+            data = pd.DataFrame(index=_get_datetime_range(start, end), columns=data.columns)
+        data["month"] = pd.to_datetime(data.index)
+        return data.reset_index(drop=True)
+
+
+    def _request_to_geocoder(string, api_key="8c1a744e-abae-4b1f-a292-076e70d90d92"):
+        lat_1, lon_1 = 41.755060, 100.567964
+        lat_2, lon_2 = 78.300151, 190.346714
+        query = f"https://geocode-maps.yandex.ru/1.x?apikey={api_key}&geocode={string}&format=json&bbox={lon_1},{lat_1}~{lon_2},{lat_2}"
+        res = requests.get(query)
+        return json.loads(res.content)
+
+
+    def predict_by_id(self, id: str):
+        row = self.buildings[id]
+        row = {
+                key: _replace_nan(value) for key, value in row.items()
+            }
+        return self.predict(row)
 
     def predict(self, row: dict):
-        pd.DataFrame(row)
-        return self.train_predictions.loc[geocoder_address]
+        columns = [
+            "weatherMin",
+            "weatherMax",
+            "weatherAvg",   
+            "precipitation",
+        ]
 
+        amount_money_columns = [
+            "amount_money_kr", # kr
+            "amount_money_ks", # ks
+            "amount_money_tr", # tr
+        ]
+
+        now = datetime.datetime.now() - datetime.timedelta(days=365*3)
+        start = datetime.datetime(year=now.year, month=now.month, day=1)
+        end = start + datetime.timedelta(days=365 * 3)
+        end = datetime.datetime(year=end.year, month=end.month, day=1)
+
+        row = {self.front_to_model[key]: value for key, value in row.items()}
+
+        lat = row["geocoder_lat"]
+        lon = row["geocoder_lon"]
+        df = self._request_to_weather_api(start, end, lat, lon)
+        print(df.info())
+        for key, value in row.items():
+            df[key] = value
+            if key in amount_money_columns:
+                df[key] = value / len(df)
+        print(df.info())
+        prediction = self.model.predict(df[self.features])
+        result = []
+
+        for i, row in df.iterrows():
+            answer = dict()
+            answer["prediction"] = prediction[i]
+            answer["date"] = row["month"]
+            for column in columns:
+                answer[column] = row[self.front_to_model[column]]
+            answer = {
+                key: _replace_nan(value) for key, value in answer.items()
+            }
+            result.append(answer)
+        return result
 
     
 
